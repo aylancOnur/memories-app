23b661e8dbed3b6f671732b14e979011fcea64b6
Fr3shBrain <aylanc.onur@gmail.com>
2021-12-24 18:39:03 +0300

    Initialize Memories App

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css"
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+import {reducers} from "./reducers";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
